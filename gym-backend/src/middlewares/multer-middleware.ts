11b097ec5f5e04b5026eeffaab2333d530a64bdf
NewUser <mraflikurniawan20@gmail.com>
2025-05-13 12:13:25 +0700

    adding multer middleware

---
+++ gym-backend/src/middlewares/multer-middleware.ts
@@ -0,0 +1,60 @@
+import { NextFunction, Request, Response } from 'express';
+import multer from 'multer';
+
+const whitelisted = ['image/png', 'image/jpeg', 'image/jpg'];
+
+const avatarStorage = multer.diskStorage({
+  destination: '/src/uploads/avatar',
+  filename(req, file, callback) {
+    const randomNumber = Math.round(Math.random() * 1000) + 1000;
+    callback(null, `${new Date(Date.now())} - ${randomNumber} - ${file.originalname}`);
+  },
+});
+const headerStorage = multer.diskStorage({
+  destination: '/src/uploads/header',
+  filename(req, file, callback) {
+    const randomNumber = Math.round(Math.random() * 1000) + 1000;
+    callback(null, `${new Date(Date.now())} - ${randomNumber} - ${file.originalname}`);
+  },
+});
+
+const uploadAvatar = multer({
+  storage: avatarStorage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (!whitelisted.includes(file.mimetype)) {
+      return cb(new Error('File not supported, only support .png, .jpg, .jpeg'));
+    } else {
+      cb(null, true);
+    }
+  },
+});
+
+const imageUpload = uploadAvatar.single('image');
+export const handleImageUpload = (req: Request, res: Response, next: NextFunction) => {
+  imageUpload(req, res, (error: any) => {
+    if (error) {
+      req.resume();
+      res.status(400).json({ error: error.message });
+    }
+    next();
+  });
+};
+const storage = multer.diskStorage({
+  destination: 'src/uploads',
+  filename: (req, file, cb) => {
+    const randomNumber = Math.round(Math.random() * 1000) + 1000;
+    cb(null, `${Date.now()}-${randomNumber}-${file.originalname}`);
+  },
+});
+export const uploads = multer({
+  storage: storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (!whitelisted.includes(file.mimetype)) {
+      return cb(new Error('File not supported, only support .png, .jpeg. .jpg'));
+    } else {
+      cb(null, true);
+    }
+  },
+});
--- gym-backend/src/routers/user-router.ts
+++ gym-backend/src/routers/user-router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { addMember, getMember } from '../controllers/user-controller';
+import { addMember, getMember, loginMember } from '../controllers/user-controller';
+import { uploads } from '../middlewares/multer-middleware';
 
 export const routerUser = Router();
 
 routerUser.post('/addmember', addMember);
+routerUser.post('/loginMember', uploads.none(), loginMember);
 routerUser.get('/getMember/:id', getMember);
